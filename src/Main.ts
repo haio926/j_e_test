868c4146d156e5f64229339dbb635b96fa52cadd
hao.zheng <hao.zheng@17zuoye.com>
2019-04-03 18:43:59 +0800

    c0

--- src/Main.ts
+++ src/Main.ts
@@ -35,8 +35,7 @@ class Main extends eui.UILayer {
     private loadingView: LoadingUI;
     protected createChildren(): void {
         super.createChildren();
-        console.log(1);
-        console.log(2);
+        console.log("c0");
         egret.lifecycle.addLifecycleListener((context) => {
             // custom lifecycle plugin
         })
